ef0314620e2df91d7bf84219d874894f559f7b16
ereinoso <edgardojesus16@gmail.com>
2025-03-17 06:25:50 +0100

    refactor(main) 🔨: Adapted main file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-from application.cli import player_service
+from application.cli import main
 
 def handler(event, context):
     main(event)
@@ -9,7 +9,4 @@ def handler(event, context):
     }
 
 if __name__ == "__main__":
-    player_name = "pedri"
-    attributes = "goals, match_date"
-    # print(player_service.get_player_stats_from_ddb(player_name, attributes))
-    print(player_service.update_ddb_table_with_latest_player_data())
+    main()
