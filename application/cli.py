460ebdad5c3260de45b7fc1515b13651d48d3fe6
ereinoso <edgardojesus16@gmail.com>
2025-03-17 06:16:25 +0100

    refactor(cli) 🔨: Init parameter definition in cli

--- application/cli.py
+++ application/cli.py
@@ -1,3 +1,4 @@
+import sys
 # application/cli.py
 from adapters.dynamodb_adapter import DynamoDBPlayerStatsRepository
 from adapters.uefa_adapter import UEFAPlayerStatsRepository
@@ -12,4 +13,25 @@ uefa_repository = UEFAPlayerStatsRepository(endpoint_url="/en/uclfantasy/service
 
 # Initialize services
 player_service = PlayerService(manual_players_repository, uefa_repository)
-print('test from cli')
+
+def get_parameters(event=None):
+    """Determine execution mode and fetch parameters accordingly."""
+    if event:
+        # Running in Lambda
+        remove_ddb_table = event.get('ddb_recreate')
+        ap_type = event.get('ap_type')
+        execution_environment = event.get('execution_environment')
+    else:
+        # Running local
+        if len(sys.argv) < 4:
+            print('Usage: uv run <ddb_recreate> <ap_type> <execution_environment>')
+            sys.exit(1)
+        remove_ddb_table = sys.argv[1]
+        ap_type = sys.argv[2]
+        execution_environment = sys.argv[3]
+    
+    return remove_ddb_table, ap_type, execution_environment
+
+def main():
+    remove_ddb_table, ap_type, execution_environment = get_parameters()
+    print(f'{remove_ddb_table} {ap_type} {execution_environment}')
