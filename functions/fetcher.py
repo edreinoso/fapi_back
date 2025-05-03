5446e9e2b8fba102cb24914ef24add7b1a185131
edreinoso <edgardojesus16@gmail.com>
2025-05-03 23:45:40 +0200

    refactor(fetcher) 🔨: Removed visualize function

--- functions/fetcher.py
+++ functions/fetcher.py
@@ -1,34 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def visualize_data_in_matplotlib(player_data: dict, attributes: str):
-    plt.figure(figsize=(10, 5))
-    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']  # Different colors for attributes
-    line_styles = ['-', '--', '-.', ':']  # Different line styles for variation
-
-    for player_index, (player_name, player_stats) in enumerate(player_data.items()):
-        if not player_stats:
-            continue # Skip players with no data
-        
-        color = colors[player_index % len(colors)]  # Assign a color per player
-
-        for attr_index, attribute in enumerate(attributes):
-            values = [int(d.get(attribute, 0)) for d in player_stats]
-
-            matches = np.arange(1, len(values) + 1)
-            linestyle = line_styles[attr_index % len(line_styles)] # Cycle through line styles
-
-            plt.plot(matches, values, marker='o', linestyle=linestyle, color=color, label=f"{player_name} - {attribute}")
-
-    plt.xlabel("Match Round")
-    plt.ylabel(attribute.capitalize())  # Dynamic label based on chosen attribute
-    plt.title(f"Comparison of {', '.join(attributes).capitalize()} per Match Round")
-    plt.xticks(matches)
-    plt.legend()
-    plt.grid(True)
-
-    plt.show()
-
 def main(event):
     """Query items in ddb database"""
     # 🌟 Welcoming message
