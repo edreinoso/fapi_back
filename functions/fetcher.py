b6e6883768d7134763ca9705568f5d37390a2d27
edreinoso <edgardojesus16@gmail.com>
2025-05-03 23:59:09 +0200

    refactor(fetcher) 🔨: Changed fetcher to use service

--- functions/fetcher.py
+++ functions/fetcher.py
@@ -1,23 +1,51 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import os
+import sys
+from src.python.adapters.dynamodb_adapter import DynamoDBPlayerStatsRepository
+from src.python.core.player_service import PlayerService
 
-def main(event):
+players_table_name = os.environ.get("PLAYERS_TABLE_NAME")
+
+# Initialize repositories
+players_repository = DynamoDBPlayerStatsRepository(table_name=players_table_name)
+
+def get_parameters(event=None) -> tuple[str, str]:
+    """Determine execution mode and fetch parameters accordingly."""
+    if event:
+        # Running in Lambda
+        player_name = event.get('player_name')
+        player_attribute = event.get('player_attribute')
+    else:
+        # Running local
+        if len(sys.argv) < 4:
+            print('Usage: uv run <player_name> <player_attribute> <execution_environment>')
+            sys.exit(1)
+        player_name = sys.argv[1]
+        player_attribute = sys.argv[2]
+    
+    return player_name, player_attribute
+
+def fetch(event):
     """Query items in ddb database"""
     # 🌟 Welcoming message
     print("\n⚽ Welcome to the UEFA Fantasy Data Explorer! ⚽")
     print("You can retrieve stats for any player!\n")
 
-    # k. mbappé, rodrydgo
+    player_name, player_attribute = get_parameters(event)
+    print(f'Player name: {player_name}, Player attribute: {player_attribute}')
 
-    # 📝 User input for player name and attribute
-    player_name = input("Enter player names (comma-separated): ").strip().split(', ')
-    attributes = input("Enter the attribute to retrieve (e.g., goals, assists, points): ").strip().split(', ')
-    attributes_str = ', '.join(attributes)  # Convert list back into a string
-    data = read_player_from_ddb(player_name, attributes_str)
+    # Initialize services
+    player_service = PlayerService(players_repository)
+    return player_service.get_player_stats_from_ddb(player_name, player_attribute)
 
-    print(data)
 
-    if data:
-        visualize_data_in_matplotlib(data, attributes)  # Pass the dictionary to the function
-    else:
-        print("No data found for the given players.")
+def handler(event, context):
+    """AWS Lambda handler function."""
+    player_data = fetch(event)
+    return {
+        'statusCode': 200,
+        'body': player_data
+    }
+
+if __name__ == "__main__":
+    fetch()
+    
