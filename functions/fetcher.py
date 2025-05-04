334d744cf7f0548fc01f16676a58d6ff5687a947
edreinoso <edgardojesus16@gmail.com>
2025-05-04 10:27:50 +0200

    refactor(fetcher) 🔨: Removed get_params function

--- functions/fetcher.py
+++ functions/fetcher.py
@@ -8,29 +8,14 @@ players_table_name = os.environ.get("PLAYERS_TABLE_NAME")
 # Initialize repositories
 players_repository = DynamoDBPlayerStatsRepository(table_name=players_table_name)
 
-def get_parameters(event=None) -> tuple[str, str]:
-    """Determine execution mode and fetch parameters accordingly."""
-    if event:
-        # Running in Lambda
-        player_name = event.get('player_name')
-        player_attributes = event.get('player_attributes')
-    else:
-        # Running local
-        if len(sys.argv) < 4:
-            print('Usage: uv run <player_name> <player_attributes>')
-            sys.exit(1)
-        player_name = sys.argv[1]
-        player_attributes = sys.argv[2]
-    
-    return player_name, player_attributes
-
 def fetch(event):
     """Query items in ddb database"""
     # 🌟 Welcoming message
     print("\n⚽ Welcome to the UEFA Fantasy Data Explorer! ⚽")
     print("You can retrieve stats for any player!\n")
 
-    player_name, player_attributes = get_parameters(event)
+    player_name = event['player_name']
+    player_attributes = event['player_attributes']
     print(f'Player name: {player_name}, Player attribute: {player_attributes}')
 
     # Initialize services
